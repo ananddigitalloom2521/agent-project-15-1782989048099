0e1b78af94d12311cf2d633f66ef93a36e837784
ananddigitalloom2521 <ananddigitalloom@gmail.com>
2026-07-02 16:14:11 +0530

    Add src/index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const indexRoutes = require('./routes/index');
+app.use('/', indexRoutes);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
